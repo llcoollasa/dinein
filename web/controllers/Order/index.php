edcc87a7907d63caf44336b14673137a92b568a7
lasa <indrajith@netstarter.com>
2017-02-07 00:54:00 +0530

    add order placing fucntions

--- web/controllers/Order/index.php
+++ web/controllers/Order/index.php
@@ -32,3 +32,86 @@ $app->match('/Order', function () use ($app) {
 
     return $app['twig']->render('Order/create.html.twig', array('data'=> $initial_data));
 })->bind('Order');
+
+$app->match('/Order/place', function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+
+    $vars = $request->request->all();
+    $items = $request->request->get('item_id');
+    $date = date("Y-m-d");
+    $basket = array();
+    $orderId =0;
+    $total = 0;
+
+    foreach($items as $itemK=>$item){
+
+        $itemAttributes = explode('_',$item);
+
+        foreach($itemAttributes as $itemAttributeK=>$itemAttribute){
+            $itemValues = explode('=',$itemAttribute);
+            $basket[$itemK][$itemValues[0]]=  $itemValues[1];
+        }
+    }
+
+    //Customer
+    //`Customer_Contact`, `Customer_Name`, `Customer_Delivery_address`, `Customer_email`
+
+    $sql_customer = "INSERT INTO Customer
+                      VALUES ('".$vars['Customer_Contact']."',
+                      '".$vars['Customer_Name']."',
+                      '".$vars['Customer_Delivery_address']."',
+                      '".$vars['Customer_email']."'
+                      )";
+
+    $app['db']->executeUpdate($sql_customer);
+    //Purchase_Order
+    //`Order_ID`, `Order_Date`, `Order_Delivery_date`, `order_type`
+
+    $sql_purchase_order = "INSERT INTO Purchase_Order(Order_Date,Order_Delivery_date,order_type)
+                              VALUES ('".$date."',
+                              '".$date."',
+                              'TAKEAWAY'
+                              )";
+
+    $app['db']->executeUpdate($sql_purchase_order);
+    $orderId = $app['db']->lastInsertId();
+
+
+    //Purchase_Order_Item
+    //`Order_ID`, `ItemIt_ID`, `Quantity`
+
+    foreach ($basket as $itemK=>$item) {
+
+        $sql_purchase_items = "INSERT INTO Purchase_Order_Item(Order_ID,ItemIt_ID,Quantity)
+                              VALUES ('".$orderId."',
+                              '".$itemK."',
+                              '".$item['itemQty']."'
+                              )";
+        $total += $item['itemSubTotal'];
+        $app['db']->executeUpdate($sql_purchase_items);
+
+    }
+
+
+
+    //Purchase_Order_Payment
+    //`Payment_Id`, `Order_ID`, `Amount`, `Payment_Date`, `Customer_Contact`, `Payment_Type`
+
+    $sql_purchase_order = "INSERT INTO Purchase_Order_Payment(Order_ID,Amount,Payment_Date,Customer_Contact,Payment_Type)
+                              VALUES ('".$orderId."',
+                              '".$total."',
+                              '".$date."',
+                              '".$vars['Customer_Contact']."',
+                              '".$vars['Payment_Type']."'
+
+                              )";
+
+    $app['db']->executeUpdate($sql_purchase_order);
+
+   return $app['twig']->render('Order/place.html.twig', array(
+        'data'=> $vars,
+        'orderId' =>$orderId,
+        'total' =>$total,
+        'basket' =>$basket
+    ));
+
+})->bind('Order_place');
